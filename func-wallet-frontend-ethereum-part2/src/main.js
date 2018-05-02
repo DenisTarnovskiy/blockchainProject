84ba1aa9a1e468be343ec0b75141ecaed2719f2a
Denis Tarnovsky <>
2018-05-02 12:43:58 +0300

    fb+bt

--- func-wallet-frontend-ethereum-part2/src/main.js
+++ func-wallet-frontend-ethereum-part2/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
+import Web3 from 'web3'
 
 Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+
+window.addEventListener('load', () => {
+    if (typeof web3 !== 'undefined') {
+        console.log('Web3 injected browser: OK.')
+        window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+        console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+        window.web3 = new Web3(new Web3.providers.HttpProvider('http://0.0.0.0:8080/'))
+    }
 })
